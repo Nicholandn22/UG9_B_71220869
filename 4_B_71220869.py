442cf4a50e924e6984df9aa72f6e97b4af62b5c4
Nicholandn22 <117884361+Nicholandn22@users.noreply.github.com>
2022-11-17 14:20:36 +0700

    Add files via upload

--- 4_B_71220869.py
+++ 4_B_71220869.py
@@ -1,26 +1,49 @@
 print ("<<<<<<<<<<<<< Adventure of Alan >>>>>>>>>>")
 print (" Welcome to the Adventure of Alan")
-gender = input("kamu laki-laki atau Perempuan (L/P): ")
+gender = input("kamu laki-laki atau Perempuan (L/P): \n>>>")
+if gender == 'L':
+    print("wahhhh kamu keceee bangettt")
+else:
+    print("wahhh kamu cantikk dan manis <3 ")
 
 print ("")
 print ("")
 print ("")              
 print ("")
-input("suatu hari alan sedang melihat uang, jadi dia berpikir ingin mengambil atau tidak (ambil/tidak)")
-
+tindakan = input("suatu hari alan sedang melihat uang, jadi dia berpikir ingin mengambil atau tidak (ambil/tidak) \n >>>")
+if tindakan == 'ambil':
+    print("alan nakall, kembalikann cepatt....")
+else:
+    print("pintarr, alan anak baik yaa...")
 print ("")
 print ("")
 print ("")
 
 print ("")
-input("alan melhat cewe dan melakukan ... (menghampiri/bersiul)")
+a = input("alan melhat cewe dan melakukan ... (menghampiri/bersiul) \n>>>")
+if a == 'bersiul':
+    print ("cewe nya melambaikan tangan juga")
+else:
+    print ("cewe nya ilfeel dan meninggalkan alan sendirian")
 
 print ("")
 print ("")
 print ("")
 print ("")
 print ("")
-input("alan melhat pimzy dan melakukan ... (menggoda/bersiul)")
+b = input("alan melhat pimzy dan melakukan ... (tos/salaman) \n>>>")
+if b == 'tos':
+    print ("mereka saling tos dan menyapa")
+else:
+    print ("mereka salaman dan menyapa")
+
+print ("\n\n\n\n\n")
 
 
-input("alan melhat lisa dan melakukan ... (minta tanda tangan/foto bareng)")
+c = input("alan melhat lisa dan melakukan ... (minta tanda tangan/fotbar) \n>>>")
+if c == 'fotbar':
+    print ("alan dapat fotbar dan di print lalu di tempel di kamar")
+else:
+    print ("alan membingkai tanda tangan tersebut dan menyimpan di kamar")
+    
+
